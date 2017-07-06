fe5f89373dcdc215f897a76b87786405c30940e9
zhangdy <dlzdy@163.com>
2017-07-06 17:30:39 +0800

    init

--- doctor-iphone/scan/ScanViewController.m
+++ doctor-iphone/scan/ScanViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ScanViewController.h"
-#import ""
-@interface ScanViewController ()
-
+@interface ScanViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@property (strong,nonatomic)AVCaptureDevice *device;
+@property (strong,nonatomic)AVCaptureDeviceInput *input;
+@property (strong,nonatomic)AVCaptureMetadataOutput *output;
+@property (strong,nonatomic)AVCaptureSession *session;
+@property (strong,nonatomic)AVCaptureVideoPreviewLayer *preview;
 @end
 
 @implementation ScanViewController
@@ -24,6 +27,9 @@
     [btnScan addTarget:self action:@selector(btnScanClick) forControlEvents:UIControlEventTouchUpInside];
     //self.view.backgroundColor= [UIColor greenColor];
     [self.view addSubview:btnScan];
+
+    [self setupCapture];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,47 +43,70 @@
 - (void)btnScanClick {
     //打开二维码扫描
     NSLog(@"开始二维码扫描");
+
+    [self.view.layer insertSublayer:_preview atIndex:0];
+    
+    // 10、启动会话
+    [_session startRunning];
+}
+
+/**
+ * 初始化摄像头
+ */
+- (void)setupCapture {
     // 1、获取摄像设备
-    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
     // 2、创建设备输入流
-    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
+    _input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
     
     // 3、创建数据输出流
-    AVCaptureMetadataOutput *metadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    _output = [[AVCaptureMetadataOutput alloc] init];
     
     // 4、设置代理：在主线程里刷新
-    [metadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
     
     // 设置扫描范围（每一个取值0～1，以屏幕右上角为坐标原点）
     // 注：微信二维码的扫描范围是整个屏幕，这里并没有做处理（可不用设置）
-    metadataOutput.rectOfInterest = CGRectMake(0.05, 0.2, 0.7, 0.6);
+    _output.rectOfInterest = CGRectMake(0.05, 0.2, 0.7, 0.6);
     
     // 5、创建会话对象
     _session = [[AVCaptureSession alloc] init];
     // 会话采集率: AVCaptureSessionPresetHigh
-    _session.sessionPreset = sessionPreset;
+    _session.sessionPreset = AVCaptureSessionPresetHigh;
     
     // 6、添加设备输入流到会话对象
-    [_session addInput:deviceInput];
+    if ([_session canAddInput:_input]) {
+        [_session addInput:_input];
+    }
     
     // 7、添加设备输入流到会话对象
-    [_session addOutput:metadataOutput];
+    if ([_session canAddOutput:_output]) {
+        [_session addOutput:_output];
+    }
     
     // 8、设置数据输出类型，需要将数据输出添加到会话后，才能指定元数据类型，否则会报错
     // 设置扫码支持的编码格式(如下设置条形码和二维码兼容)
-    // @[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code,  AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code]
-    metadataOutput.metadataObjectTypes = metadataObjectTypes;
+    //    NSArray *metadataObjectTypes =
+    //        [[NSArray alloc]initWithObjects:@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code,  AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code]];
+    //    _output.metadataObjectTypes = metadataObjectTypes;
+    
+    _output.metadataObjectTypes = @[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code,  AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];//可以运行，不错
     
     // 9、实例化预览图层, 传递_session是为了告诉图层将来显示什么内容
-    _videoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+    _preview = [AVCaptureVideoPreviewLayer layerWithSession:_session];
     // 保持纵横比；填充层边界
-    _videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    _videoPreviewLayer.frame = currentController.view.layer.bounds;
-    [currentController.view.layer insertSublayer:_videoPreviewLayer atIndex:0];
-    
-    // 10、启动会话
-    [_session startRunning];
+    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _preview.frame = self.view.layer.bounds;
+}
+/**
+ * 扫描成功返回字符串
+ */
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+    if (metadataObjects.count > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        NSLog(@"scan result = %@", metadataObj.stringValue);
+    }
 }
 /*
 #pragma mark - Navigation
